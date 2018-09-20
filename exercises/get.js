d1adc5ce6e049cea037efc48711582a141fe1a22
Zakaria Hamichi <Hamichi.zakaria@gmail.com>
2018-09-20 17:25:07 +0200

    TDD3

--- exercises/get.js
+++ exercises/get.js
@@ -10,15 +10,8 @@
 // Provided code :
 
 // Your code :
-let a = 0
+
 
 //* Begin of tests
-const assert = require('assert')
 
-assert.strictEqual(typeof get, 'function')
-assert.strictEqual(get('num'), 42)
-assert.strictEqual(get('bool'), true)
-assert.strictEqual(get('str'), 'some text')
-assert.strictEqual(get('log'), console.log)
-assert.strictEqual(get('noexist'), undefined)
 // End of tests */
